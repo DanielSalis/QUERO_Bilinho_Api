ea8f9a8d7c376092614b8bc63571f97de4262c75
DanielSalis <danielsalis98@gmail.com>
2021-10-04 17:46:45 -0300

    adding create method to AlunosController

--- app/controllers/api/v1/alunos_controller.rb
+++ app/controllers/api/v1/alunos_controller.rb
@@ -9,6 +9,28 @@ module Api
                     data: types
                 }, status: :ok
             end
+
+            def create
+                aluno = Aluno.new(alunos_params)
+
+                if aluno.save
+                    render json: {
+                        status: 'Success',
+                        message: 'Aluno criado com sucesso',
+                        data: aluno
+                    }, status: :ok
+                else
+                    render json: {
+                        status: 'ERROR',
+                        message:'Aluno não pode ser criado',
+                        data:article.erros}, status: :unprocessable_entity
+				end
+			end
+            
+            private
+            def alunos_params
+                params.permit(:nome, :cpf, :data_nascimento, :telefone_celular, :tipo_generos_id, :meio_pagamentos_id)
+            end
         end    
     end
 end
