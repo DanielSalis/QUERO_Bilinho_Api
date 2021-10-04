40e1c8b4211db7742c1feb0bb282995d3bbd9219
DanielSalis <danielsalis98@gmail.com>
2021-10-04 15:05:49 -0300

    fixing validation

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -2,15 +2,11 @@ class Aluno < ApplicationRecord
     validates :nome, 
         presence: true, 
         uniqueness: true
-    validades :cpf, 
+    validates :cpf, 
         presence: true,
         uniqueness: true,
         length: {is: 11},
         numericality: { only_integer: true }
-    validades :data_nascimento,
-        allow_blank: true
-    validades :telefone_celular
-        allow_blank: true
     
     has_one :TipoGenero
     has_one :MeioPagamento 
